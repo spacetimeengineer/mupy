21035e548cbe9ddca1a9356431a08c0d03c51b96
Michael Ryan <spacetime.engineer@gmail.com>
2022-08-29 20:47:25 -0400

    test

--- examples/gearbox.py
+++ examples/gearbox.py
@@ -12,24 +12,30 @@ import mupy.core as mu # Imports mupy library.
 workspace_name = "gear_box_test" # Workspace name ; this will be the name of the workspace directory.
 workspace = mu.WorkSpace(str(Path.home())+"/"+workspace_name) # Creates workspace.
 
-block_length = 12.5
-shaft_radius = 4
+""" Parametrization"""
+
+block_length = 7.5
+shaft_radius = 2.18
 x_block_units = 8
 y_block_units = 8
+
+""" System-Code Formating"""
 
 block_length = str(block_length).replace(".", "P", 1)
 shaft_radius = str(shaft_radius).replace(".", "P", 1)
 x_block_units = str(x_block_units)
 y_block_units = str(y_block_units)
+bearing_insert_radius_mm = 16
 
 """ Define Hardware Components"""
+
 panel_c = mu.Hardware("panel_c", "CUBX0177-BPAN-B"+block_length+"SR"+shaft_radius+"-X"+x_block_units+"Y"+y_block_units+"PP2-RF-SX25Y25-X1Y1-XO0YO0-X30Y30Z20-C")
 panel_d = mu.Hardware("panel_d", "CUBX0177-BPAN-B"+block_length+"SR"+shaft_radius+"-X"+x_block_units+"Y"+y_block_units+"PP2-RF-SX25Y25-X1Y1-XO0YO0-X30Y30Z20-C")
 panel_e = mu.Hardware("panel_e", "CUBX0177-BPAN-B"+block_length+"SR"+shaft_radius+"-X"+x_block_units+"Y"+y_block_units+"PP2-RF-SX25Y25-X1Y1-XO0YO0-X30Y30Z20-C")
 panel_f = mu.Hardware("panel_f", "CUBX0177-BPAN-B"+block_length+"SR"+shaft_radius+"-X"+x_block_units+"Y"+y_block_units+"PP2-RF-SX25Y25-X1Y1-XO0YO0-X30Y30Z20-C")
 
-panel_a = mu.Hardware("panel_a", "CUBX0177-BPAN-B12P5SR4-X8Y8PP18-RF-SX25Y25-X1Y1-XO0YO0-X30Y30Z20-C")
-bevel_gear_a = mu.Hardware("bevel_gear_a", "CRSPGR022-BG-M2-T34W25-P15H0C45-B0")
+panel_a = mu.Hardware("panel_a", "CUBX0177-BPAN-B"+block_length+"SR"+shaft_radius+"-X8Y8PP18-RF-SX25Y25-X1Y1-XO0YO0-X16Y16Z20-C")
+bevel_gear_a = mu.Hardware("bevel_gear_a", "CRSPGR022-BG-M1P75-T20W9-P15H0C45-B4P9")
 bevel_gear_a.color="blue"
 
 panel_gear_assembly_a = mu.Assembly("panel_gear_assembly_a")
@@ -48,8 +54,8 @@ panel_assembly_a = mu.Assembly("panel_assembly_a")
 panel_assembly_a.include(panel_gear_assembly_a, mu.Coordinates(0.4,0.9,[-100,0,0],[-50,0,0],[0,180,0],[0,180,0]))
 panel_assembly_a.include(axle_adapter_flywheel_adapter_assembly_a, mu.Coordinates(0.4,0.9,[100,0,0],[50,0,0],[0,0,0],[0,0,0]))
 
-panel_b = mu.Hardware("panel_b", "CUBX0177-BPAN-B12P5SR4-X8Y8PP18-RF-SX25Y25-X1Y1-XO0YO0-X30Y30Z20-C")
-bevel_gear_b = mu.Hardware("bevel_gear_b", "CRSPGR022-BG-M2-T34W25-P15H0C45-B0")
+panel_b = mu.Hardware("panel_b", "CUBX0177-BPAN-B"+block_length+"SR"+shaft_radius+"-X8Y8PP18-RF-SX25Y25-X1Y1-XO0YO0-X16Y16Z20-C")
+bevel_gear_b = mu.Hardware("bevel_gear_b", "CRSPGR022-BG-M1P75-T20W9-P15H0C45-B4P9")
 
 panel_gear_assembly_b = mu.Assembly("panel_gear_assembly_b")
 panel_gear_assembly_b.include(panel_b, mu.Coordinates(0.1,0.4,[-100,0,0],[-18.5,0,0],[180,90,0],[180,90,0]))
@@ -66,11 +72,8 @@ panel_assembly_b = mu.Assembly("panel_assembly_b")
 panel_assembly_b.include(panel_gear_assembly_b, mu.Coordinates(0.4,0.9,[-50,0,0],[-50,0,0],[0,180,0],[0,180,0]))
 panel_assembly_b.include(axle_adapter_flywheel_adapter_assembly_b, mu.Coordinates(0.4,0.9,[300,0,0],[50,0,0],[0,0,0],[0,0,0]))
 
-
-
 bearing_security_cap = mu.Hardware("bearing_security_cap_b","CUBX0177-BPANS2-B7P5SR2P2-X10Y10PP21-RF-SX0Y0-X1Y1-XO0YO0-X45Y45Z6-S-SX0Y0-X1Y1-XO0YO0-X12Y12Z15-S")
-
-
+#bearing_security_cap = mu.Hardware("CUBX0177-SPANS2-B7P5SR2P2-X6Y6-SX0Y0-X1Y1-XO0YO0-X18Y18Z0P75-C-SX0Y0-X1Y1-XO0YO0-X7P5Y7P5Z10-S")
 
 alpha = 200 
 gearbox_assembly = mu.Assembly("gearbox_assembly")
